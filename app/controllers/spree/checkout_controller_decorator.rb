e5c10084b3a9ad32ad671ea28d8d97e8b59f0d50
Marcus Wermuth <marcus@mwermuth.com>
2014-03-09 12:16:02 +0100

    Add new load order

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -4,7 +4,7 @@ module Spree
 
     private
     def remove_payments_attributes_if_total_is_zero
-      load_order
+      load_order_with_lock
 
       return unless params[:order] && params[:order][:store_credit_amount]
       store_credit_amount = [BigDecimal.new(params[:order][:store_credit_amount]), spree_current_user.store_credits_total].min
